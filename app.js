faa3ab3ce0df74aa9d18001a5e1b08545e3f3162
FriendlyUser1 <kitty.kat5532@gmail.com>
2022-11-30 22:55:42 +0000

    notifs

--- app.js
+++ app.js
@@ -19,6 +19,23 @@ const addItem = () => {
 		id = form.elements[0].value,
 		answer = form.elements[1].value;
 	(form.elements[0].value = ""), (form.elements[1].value = "");
+};
 
-	console.log(id, answer);
+// output
+
+const outputNode = document.getElementsByClassName("output")[0];
+
+const output = (isError, message) => {
+	if (outputNode.children.length > 0) clearOutput();
+	outputNode.style.backgroundColor = isError
+		? "rgba(247, 60, 60, 0.486)"
+		: "rgba(27, 255, 57, 0.37)";
+	outputNode.parentElement.style.display = "flex";
+	outputNode.appendChild(document.createElement("p"));
+	outputNode.children[0].appendChild(document.createTextNode(message));
+};
+
+const clearOutput = () => {
+	document.getElementsByClassName("output-container")[0].style.display = "none";
+	outputNode.removeChild(outputNode.children[0]);
 };
